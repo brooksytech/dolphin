0dad9f6a43ac766bdf1b8352e0bc0b88e724283f
Brooksytech <macebrooks@gmail.com>
2023-05-28 13:28:29 -0700

    Update GLContext.cpp

--- Source/Core/Common/GL/GLContext.cpp
+++ Source/Core/Common/GL/GLContext.cpp
@@ -19,6 +19,10 @@
 #endif
 #if HAVE_EGL
 #include "Common/GL/GLInterface/EGL.h"
+#if HAVE_DRM
+  else if (wsi.type == WindowSystemType::DRM)
+    context = std::make_unique<GLContextEGLDRM>();
+#endif
 #if HAVE_X11
 #include "Common/GL/GLInterface/EGLX11.h"
 #endif
